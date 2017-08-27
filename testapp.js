65778851d9c6186c0847f879bd9e582cddf3bd50
Anthony Shinn <ayshinn@umich.edu>
2017-08-27 17:51:23 -0400

    pls work

--- testapp.js
+++ testapp.js
@@ -1,31 +1,39 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
-// function coinToss() {
-//   // This function will randomly return either 'heads' or 'tails'.
-//   return Math.random() < 0.5 ? 'heads' : 'tails';
-// }
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
 
-// const pics = {
-//   kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
-//   doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
-// };
-// let img;
+const pics = {
+  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+let description;
 
-// // if/else statement begins here:
-// if (coinToss() === 'heads') {
-//   	img = (
-//     	//<img src={pics.kitty} />
-//         <img src="images/sunset.jpg" />
-//     );
-// } else {
-//     img = (
-//     	<img src="images/walk.jpg" />
-//     );
-// }
+// if/else statement begins here:
+if (coinToss() === 'heads') {
+  	img = (
+    	//<img src={pics.kitty} />
+        <img src="images/sunset.jpg" />
+    );
+    description = (<p>Sometimes I'm a sunset...</p>);
+} else {
+    img = (
+    	<img src="images/walk.jpg" />
+    );
+    description = (<p>Sometimes I'm a dune pic...</p>);
+}
+img = (<img width="200" height="200" />);
 
 ReactDOM.render(
-	<h5>please work</h5>,
+	description,
     document.getElementById('yoboi')
 );
 
+ReactDOM.render(
+	img,
+    document.getElementById('yoboi')
+);
